e7c727e14646dbf14af1aeff7bb94fcebe5dd4cd
tigra2894-jpg <tigra2894@gmail.com>
2026-09-06 19:31:59 +0200

    Create MainActivity.java

---
+++ app/src/main/java/com/marius/tetris3d/MainActivity.java
@@ -0,0 +1,39 @@
+package com.marius.tetris3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.onResume();
+    }
+}
