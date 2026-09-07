55f1b04cc19678b5e6c013fad598e7b3aa227b14
tigra2894-jpg <tigra2894@gmail.com>
2026-09-07 15:34:08 +0200

    Update Particule.java

--- app/src/main/java/com/marius/tetris3d/Particule.java
+++ app/src/main/java/com/marius/tetris3d/Particule.java
@@ -20,19 +20,36 @@ public class Particule {
     private final float[] cg = new float[MAX];
     private final float[] cb = new float[MAX];
     private final float[] marime = new float[MAX];
+    private final float[] vitezaRot = new float[MAX];
 
     private final Random rnd = new Random();
     private int urmator = 0;
 
+    /** explozia clasica: scantei portocalii-galbene */
     public void explozie(int rand, int coloane) {
         for (int c = 0; c < coloane; c++) {
             for (int k = 0; k < 4; k++) {
-                naste(c, rand);
+                naste(c, rand, false, 1f, 1f, 1f);
             }
         }
     }
 
-    private void naste(float cx, float cy) {
+    /** explozie de cioburi de sticla, colorate dupa culoarea fiecarui bloc */
+    public void explozieColorata(int rand, int coloane, int[] valRand, float[][] culori) {
+        for (int c = 0; c < coloane; c++) {
+            int val = valRand[c];
+            float r = 1f, g = 1f, b = 1f;
+            if (val > 0 && culori != null && val - 1 < culori.length) {
+                float[] cul = culori[val - 1];
+                r = cul[0]; g = cul[1]; b = cul[2];
+            }
+            for (int k = 0; k < 6; k++) {
+                naste(c, rand, true, r, g, b);
+            }
+        }
+    }
+
+    private void naste(float cx, float cy, boolean cioburi, float culR, float culG, float culB) {
         int i = urmator;
         urmator = (urmator + 1) % MAX;
 
@@ -40,19 +57,34 @@ public class Particule {
         y[i] = cy + (rnd.nextFloat() - 0.5f) * 0.8f;
         z[i] = (rnd.nextFloat() - 0.5f) * 0.6f;
 
-        vx[i] = (rnd.nextFloat() - 0.5f) * 7f;
-        vy[i] = rnd.nextFloat() * 6f + 1.5f;
-        vz[i] = (rnd.nextFloat() - 0.5f) * 5f;
+        if (cioburi) {
+            // cioburile de sticla zboara mai repede si mai lateral
+            vx[i] = (rnd.nextFloat() - 0.5f) * 10f;
+            vy[i] = rnd.nextFloat() * 5f + 1.0f;
+            vz[i] = (rnd.nextFloat() - 0.5f) * 8f;
 
-        viataMax[i] = 0.7f + rnd.nextFloat() * 0.7f;
-        viata[i] = viataMax[i];
+            viataMax[i] = 0.5f + rnd.nextFloat() * 0.5f;
+            viata[i] = viataMax[i];
 
-        float t = rnd.nextFloat();
-        cr[i] = 1.0f;
-        cg[i] = 0.55f + t * 0.45f;
-        cb[i] = 0.15f + t * 0.55f;
+            cr[i] = culR; cg[i] = culG; cb[i] = culB;
+            marime[i] = 0.10f + rnd.nextFloat() * 0.14f;
+            vitezaRot[i] = 480f + rnd.nextFloat() * 300f;
+        } else {
+            vx[i] = (rnd.nextFloat() - 0.5f) * 7f;
+            vy[i] = rnd.nextFloat() * 6f + 1.5f;
+            vz[i] = (rnd.nextFloat() - 0.5f) * 5f;
 
-        marime[i] = 0.14f + rnd.nextFloat() * 0.16f;
+            viataMax[i] = 0.7f + rnd.nextFloat() * 0.7f;
+            viata[i] = viataMax[i];
+
+            float t = rnd.nextFloat();
+            cr[i] = 1.0f;
+            cg[i] = 0.55f + t * 0.45f;
+            cb[i] = 0.15f + t * 0.55f;
+
+            marime[i] = 0.14f + rnd.nextFloat() * 0.16f;
+            vitezaRot[i] = 320f;
+        }
     }
 
     public void actualizeaza(float dt) {
@@ -72,7 +104,6 @@ public class Particule {
         }
     }
 
-    /** desenare veche, prin matrici directe */
     public void deseneaza(Cub cub, float[] vizProiectie, float[] model,
                           float[] mvp, float offX, float offY) {
         for (int i = 0; i < MAX; i++) {
@@ -83,7 +114,7 @@ public class Particule {
 
             Matrix.setIdentityM(model, 0);
             Matrix.translateM(model, 0, offX + x[i], offY + y[i], z[i]);
-            Matrix.rotateM(model, 0, (1f - p) * 320f, 0.6f, 1f, 0.4f);
+            Matrix.rotateM(model, 0, (1f - p) * vitezaRot[i], 0.6f, 1f, 0.4f);
             Matrix.scaleM(model, 0, s, s, s);
             Matrix.multiplyMM(mvp, 0, vizProiectie, 0, model, 0);
 
@@ -91,7 +122,6 @@ public class Particule {
         }
     }
 
-    /** desenare noua, prin Desenator */
     public void deseneaza2(Desenator d, float offX, float offY) {
         for (int i = 0; i < MAX; i++) {
             if (viata[i] <= 0f) continue;
@@ -100,7 +130,7 @@ public class Particule {
             float s = marime[i] * (0.35f + p * 0.65f);
 
             d.cubRotit(offX + x[i], offY + y[i], z[i],
-                    (1f - p) * 320f, 0.6f, 1f, 0.4f,
+                    (1f - p) * vitezaRot[i], 0.6f, 1f, 0.4f,
                     cr[i], cg[i], cb[i], p, s);
         }
     }
