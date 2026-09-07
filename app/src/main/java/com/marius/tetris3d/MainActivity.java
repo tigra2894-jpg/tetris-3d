6966658a79f7d84f54da5a3b9440ed77b984262b
tigra2894-jpg <tigra2894@gmail.com>
2026-09-07 10:49:46 +0200

    Update MainActivity.java

--- app/src/main/java/com/marius/tetris3d/MainActivity.java
+++ app/src/main/java/com/marius/tetris3d/MainActivity.java
@@ -1,7 +1,6 @@
 package com.marius.tetris3d;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -9,7 +8,6 @@ import android.view.WindowManager;
 public class MainActivity extends Activity {
 
     private GameView gameView;
-    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +21,6 @@ public class MainActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-        prefs = getSharedPreferences("tetris3d", MODE_PRIVATE);
-
         gameView = new GameView(this);
         setContentView(gameView);
     }
@@ -33,7 +29,7 @@ public class MainActivity extends Activity {
     protected void onPause() {
         super.onPause();
         gameView.onPause();
-        salveazaRecord();
+        gameView.laPauzaAplicatie();
     }
 
     @Override
@@ -42,16 +38,10 @@ public class MainActivity extends Activity {
         gameView.onResume();
     }
 
-    private void salveazaRecord() {
-        // recordul se citeste din joc si se scrie in memoria telefonului
-        try {
-            java.lang.reflect.Field f = GameView.class.getDeclaredField("renderer");
-            f.setAccessible(true);
-            GameRenderer rend = (GameRenderer) f.get(gameView);
-            if (rend != null && rend.joc != null) {
-                int rec = Math.max(rend.joc.record, prefs.getInt("record", 0));
-                prefs.edit().putInt("record", rec).apply();
-            }
-        } catch (Exception ignored) { }
+    @Override
+    public void onBackPressed() {
+        if (!gameView.inapoi()) {
+            super.onBackPressed();
+        }
     }
 }
